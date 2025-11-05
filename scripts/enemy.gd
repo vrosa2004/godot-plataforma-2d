9fb7bd47bd26cab21caef047dc72bc24e5da70a7
vinicius <vinicius.rosa2004@gmail.com>
2025-11-05 18:17:16 -0300

    adicionado inimigo que anda (sem hurtbox)

---
+++ scripts/enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED = 700.0
+const JUMP_VELOCITY = -400.0
+
+@onready var wall_detector := $wall_detector as RayCast2D
+@onready var texture := $texture as Sprite2D
+
+var direction := -1
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x *= -1
+		
+	if direction == 1:
+		texture.flip_h = true
+	else:
+		texture.flip_h = false
+				
+	velocity.x = direction * SPEED * delta
+
+	move_and_slide()
