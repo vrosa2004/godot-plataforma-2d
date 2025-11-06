86be7d7338af61402868b1fe5a002828952adab7
vinicius <vinicius.rosa2004@gmail.com>
2025-11-06 15:34:51 -0300

    adicionado animação de dano ao player, vida, hurtbox e ajustado animação idle

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,11 @@ const JUMP_FORCE := -325.0
 
 @onready var anim: AnimatedSprite2D = $anim
 @onready var remote_transform := $remote as RemoteTransform2D
+@onready var ray_left := $ray_left as RayCast2D
+@onready var ray_right := $ray_right as RayCast2D
+
+var player_life := 3
+var knockback_vector := Vector2.ZERO
 
 var is_jumping := false
 
@@ -29,6 +34,9 @@ func _physics_process(delta: float) -> void:
 		anim.scale.x = sign(dir)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
 
 	# animation
 	_play_anim(dir)
@@ -44,9 +52,27 @@ func _play_anim(dir: float) -> void:
 		anim.play("idle")
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies"):
+	#if body.is_in_group("enemies"):
+		#queue_free()
+	if player_life < 0:
 		queue_free()
+	else:
+		if ray_right.is_colliding():
+			take_damage(Vector2(-200, -200))
+		elif ray_left.is_colliding():
+			take_damage(Vector2(200, -200))
 
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
+
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		anim.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property(anim, "modulate", Color(1,1,1,1), duration)
